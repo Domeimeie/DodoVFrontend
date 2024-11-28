11368ae3be1dfa32d5f26a3b2de1c7fc2a3d5ac8
Dominique <schlurpf@gmail.com>
2024-11-28 16:01:50 +0100

    Changes to initial structure

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
-  <h1>Meine Webapp</h1>
-  <TemplateRefs />
+  <h1>DodoV</h1>
+  <Login />
 </template>
 <script setup>
+  import Login from './components/login/login.vue';
   import IfForSlot from './components/if-for-slot/ifForSlot.vue';
   import Events from './components/events/events.vue';
   import TemplateRefs from './components/templateRefs/templateRefs.vue';
